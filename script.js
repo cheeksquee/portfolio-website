bb355f872084db1a81446a1c839b9b0a02b50236
cheeksquee <me.adamradwan@gmail.com>
2026-07-05 03:21:11 -0400

    position fix
    making the javascript wasn't all my work to be honest. I used other guides and even the gemini AI overview you get when you search to find ways to do certain things (e.g. how do I set this div to the mouse's position?)
    
    the way I was given worked, but it didn't really make sense to me. the way I found out now works the same, but it makes sense (the event.clientX gets the coordinate, then the + px puts that coordinate as pixels that could be read. (e.g. coordinate 250, 400 would be 250px, 400px which is the position of the box)

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener("click", (event) => {
     const box = document.createElement("div");
     box.classList.add("click_box");
 
-    box.style.left = `${event.clientX}px`;
-    box.style.top = `${event.clientY}px`;
+    box.style.left = event.clientX + "px";
+    box.style.top = event.clientY + "px";
 
     document.body.appendChild(box);
 
